32cc2d0f75a9a2f5a26609a6f6af4e041d4bc714
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-24 19:59:34 +0530

    Basics of TypeScript

---
+++ 01_Basics-Of-Typescript/01_basics.js
@@ -0,0 +1,4 @@
+var a = "Hello, I am learning Typescript";
+console.log(a);
+var b = 1003493;
+console.log(b);
---
+++ 01_Basics-Of-Typescript/01_basics.ts
@@ -0,0 +1,5 @@
+const a = "Hello, I am learning Typescript";
+console.log(a);
+
+const b = 1003493;
+console.log(b);
