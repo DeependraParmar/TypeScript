57c7ef03c80af724e90f17fd82f16d4751638be5
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-26 21:18:17 +0530

    All about arrays in ts

--- 04_Arrays/index.ts
+++ 04_Arrays/index.ts
@@ -1,3 +1,16 @@
-const arr: number[] = [];
-console.log(arr);
+const arr1: number[] = [12,12,45,36];
+const arr2:Array<string> = ['hello','world'];
+const arr3: Array<string | number> = [10,"sad",'hello',494];
 
+console.log("Number array is: ", arr1);
+console.log("String array is: ", arr2);
+console.log("Mixed array is: ", arr3);
+  
+// print 
+arr2.forEach(item => {
+    console.log(item);
+});
+
+// creating a tuple(which looks like array ) but has a fixed size.
+const arr4: [number,number,number] = [10,20,40];
+console.log("Tuple with type number is: ", arr4);
