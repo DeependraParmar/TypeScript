26129f7f0ecdc44d3beddd03888d7bbf8ae7a4dd
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-28 15:33:59 +0530

    React Hooks in TS

--- 13_React-Typescript/src/App.tsx
+++ 13_React-Typescript/src/App.tsx
@@ -1,12 +1,51 @@
-import React from "react"
-import Box from "./components/Box"
+import { FormEvent, ReactNode, createContext, useState } from "react"
+import Box from "./components/Box";
+
+interface Person{
+  name: string,
+  age: number,
+}
+
+type ThemeType = 'light' | 'dark';
+interface ThemeContextType{
+  theme: ThemeType,
+  toggleTheme: () => void,
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: 'light',
+  toggleTheme: () => {}
+});
+
+const ThemeProvider = ({children}: {children: ReactNode}) => {
+  const [theme, setTheme] = useState<ThemeType>('light');
+  
+  const toggleTheme = () => {
+    setTheme(prev => prev === "light" ? "dark" : "light");
+  }
+
+  return <ThemeContext.Provider value={{theme, toggleTheme}}>
+    {children}
+  </ThemeContext.Provider>
+}
 
 function App() {
-
+  const [user, setUser] = useState<Person>();
+  const submitHandler = (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({name: "", age: 0});
+  }
   return (
-    <>
-      <Box heading="Hello there, I am heading of the Box" count={2000} func={() => alert("Hello, a triggered function")} />
-    </>
+    <ThemeProvider>
+      <form onSubmit={submitHandler}>
+        <input type="text" value={user?.name || ""} onChange={(e) => setUser((prev) => ({...prev!, name: e.target.value}))} placeholder="Name here" /> <br />
+        <input type="number" value={user?.age || ""} onChange={(e) => setUser((prev) => ({...prev!, age: Number(e.target.value)}))} placeholder="Age here"  /> <br />
+        <button type="submit">Submit</button>
+      </form> <br /><br />
+
+      <Box label="hello" value={'hello'} setter={() => {}} />
+    </ThemeProvider>
   )
 }
 
--- 13_React-Typescript/src/components/Box.tsx
+++ 13_React-Typescript/src/components/Box.tsx
@@ -1,16 +1,20 @@
-type PropsType = {
-    heading: string,
-    count: number,
-    func: () => void,
-}
+import { Dispatch, SetStateAction, useContext } from "react";
+import { ThemeContext } from "../App";
 
-const Box = ({heading, count, func}: PropsType) => {
-    func();
+type InputValType = string | number;
+
+const Box = <T extends InputValType>({label, value, setter}: {label: string, value: T, setter: Dispatch<SetStateAction<T>>}) => {
+
+    const {theme, toggleTheme} = useContext(ThemeContext);
+    alert(theme);
   return (
-    <div>
-      <h1>{heading}</h1>
-      <p>{count}</p>
-    </div>
+    <>
+        <form>
+            <label>{label}</label>
+            <input type="text" value={value} onChange={(e) => setter(e.target.value as T)} />
+            <button onClick={toggleTheme}>Submit</button>
+        </form>
+    </>
   )
 }
 
